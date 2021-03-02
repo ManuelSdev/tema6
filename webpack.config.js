6ebddfe3ba75bb17ce88229d473137a5a002e309
Jordi <jordi@iceverg.com>
2021-03-02 23:56:31 +0100

    webpack added output

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
+
+const path = require('path');
+
+
 module.exports = {
   mode: 'production',
-  entry: './src/index.js'
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  }
 };
