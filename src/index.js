b04d0da974d806eaf5be53c6fd47f47ad68f14d7
Jordi <jordi@iceverg.com>
2021-03-02 20:52:15 +0100

    TODO #2: asked for help with generateComentSnippet

--- src/index.js
+++ src/index.js
@@ -59,6 +59,27 @@ async function drawComments(gameId) {
   }
 }
 
+function generateCommentSnippet(comment) {
+  return `<div class="row">
+    <div class="col col-2">
+      <div class="image-container text-right">
+        <img class="avatar rounded" src="${comment.user.image}" alt="${comment.user.name}" />
+      </div>
+    </div>
+    <div class="col col-10">
+      <div class="user-name-container">
+        <h4 class="text-muted">${comment.user.name}</h4>
+      </div>
+      <div class="comment-body-container">
+        ${comment.body}
+      </div>
+      <div class="date-container">
+        Comment date: <b>${comment.commentDate}</b>
+      </div>
+    </div>
+  </div>`;
+}
+
 // TODO: #3 Dar más información que una lista (imagen, nombre, primeros 100 caracteres de summary)
 function generateGameSnippet(game) {
   return `<a href="/detail.html?id=${game.id}">${game.name}</a>`;
