1709a7485f6bb2c63cfafc6beb06241cc00c88ac
Jordi <jordi@iceverg.com>
2021-03-04 21:25:06 +0100

    mapping inputs to 2 outputs

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ const path = require('path');
 
 module.exports = {
   // mode: 'development',
-  entry: ['./src/index.js', './src/detail.js'],
+  entry: {
+    main: './src/index.js',
+    detail: './src/detail.js'
+  },
   // output: {
   //   path: path.resolve(__dirname, 'dist'),
   //   filename: 'main.js'
