1b6966ca8d2f630a75e05697c75dd66124cfce43
Jordi <jordi@iceverg.com>
2021-03-04 23:34:07 +0100

    kickoff plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
@@ -32,6 +33,7 @@ module.exports = {
       }
     ],
   },
+  plugins: [new HtmlWebpackPlugin()],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     //   compress: true,
