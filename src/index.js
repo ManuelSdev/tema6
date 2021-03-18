c8ec1dae2f3394900715f0835aa018d9eae4d986
Manuel S <masanchezzm@gmail.com>
2021-03-18 11:52:17 +0100

    TODO #1: index.js= getGames+getGameById+getCommentsOfGame

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 //TODO: #1 implementar api. Mirar en documentación
+const endpoint = 'http://localhost:3000';
+
+const getGames = async () => {
+  const response = await fetch(`${endpoint}/games`);
+  return response.json();
+}
+
+const getGameById = async (gameId) => {
+  const response = await fetch(`${endpoint}/games/${gameId}`);
+  return response.json();
+}
+
+const getCommentsOfGame = async (gameId) => {
+  const response = await fetch(`${endpoint}/games/${gameId}/comments?_expand=user`);
+  return response.json();
+}
+
+
 async function drawListGames() {
     let games = await getGames();
     document.getElementById('games')
@@ -10,6 +28,8 @@ async function drawListGames() {
             .appendChild(document.createElement('li')).innerHTML = generateGameSnippet(games[i]);
     }
 }
+
+
 
 async function drawGame(gameId) {
   let game = await getGameById(gameId);
