b33551ff18a90dfde828f9aaa7a8a9c6512e13d7
Manuel S <masanchezzm@gmail.com>
2021-03-22 20:04:03 +0100

    configuro webpack.config.js con output a un index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
+//Usamos el paquete nativo de node
+const path = require('path');
+
 module.exports = {
     entry: './src/index.js',
-    mode: 'production'
+    mode: 'production',
+    output: {
+      //Le decimos que el path sea el mismo
+      path: path.resolve(__dirname, 'dist'),
+      //Pero con un nombre de archivo distinto: que sea index.js
+      filename: 'index.js',
+    },
   };
   
