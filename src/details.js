c995a5b2a679322c0e23d2d1811d4fa5607c58fc
Manuel S <masanchezzm@gmail.com>
2021-03-22 22:44:53 +0100

    separamos index.js en details.js - ponemos doble entry al webpack - ponemos modo development y production para tirar el npm run

---
+++ src/details.js
@@ -0,0 +1,35 @@
+import{getGames,getGameById,getCommentsOfGame} from './api-handler.js'
+import{generateGameSnippet, generateCommentSnippet} from './functions.js'
+
+export async function drawGame(gameId) {
+    let game = await getGameById(gameId);
+    document.getElementById('game-name-title').innerHTML = game.name;
+    // TODO: #4 breadcrumb???? Let's use jQuery!!
+    $(".breadcrumb span").html(`Game ${game.id}`);
+    document.getElementById('game-image').src = game.image;
+    document.getElementById('game-image').alt = game.name;
+  
+    document.getElementById('metacritic-score').innerHTML = game.scores.metacritic;
+    document.getElementById('user-score').innerHTML = game.scores.userScore;
+  
+    document.getElementById('summary').innerHTML = game.summary;
+  
+    document.getElementById('platform').innerHTML = game.platform;
+    document.getElementById('release-date').innerHTML = game.relaseDate;
+  
+  }
+  
+  // TODO: #2 Pedir ayuda para generateCommentSnippet
+  export async function drawComments(gameId) {
+    console.log(gameId)
+    let comments = await getCommentsOfGame(gameId);
+    document.getElementById('comments')
+        .appendChild(document.createElement('ul'))
+        .setAttribute('id', 'comments-list');
+    var i = 0;
+    for (i; i < comments.length; i++) {
+        document.getElementById('comments-list')
+            .appendChild(document.createElement('li')).innerHTML = generateCommentSnippet(comments[i]);
+    }
+  }
+  
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: ['./src/index.js', './src/details.js'],
     mode: 'production',
     output: {
       //Le decimos que el path sea el mismo
